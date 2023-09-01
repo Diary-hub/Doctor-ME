e2450cd3e459e75965424464491f169e5f2ef8ab
MohammedSafin <ms20047@auis.edu.krd>
2023-09-01 17:47:54 +0300

    starting chat page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
+import Index from './pages/Index.jsx';
 
 function App() {
 
   return (
-    <div class="wrapper">
-    <form action="">
-        <h1>DHS</h1>
-        <h4>Doctor Helping Services</h4>
-        <div class="input-box">
-            <input type="text" placeholder="Username" required/>
-            <i class='bx bxs-user'></i>
-        </div>
-        <div class="input-box">
-            <input type="password" placeholder="Password" required/>
-            <i class='bx bxs-lock-alt' ></i>
-        </div>
-        <button type="submit" class="btn">Login</button>
-    </form>
-</div>
-     
+    <Index />
   )
 }
 
---
+++ Frontend/src/pages/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/index.css'
+
+
+
+export default function Index() {
+
+  return (
+ <div>
+    <h1>Lorem ipsum dolor sit amet.</h1>
+ </div>
+  )
+}
+
+ 
