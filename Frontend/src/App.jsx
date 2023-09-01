df2800f5e8c97bc2a7c075182ab317cb1ceff053
MohammedSafin <ms20047@auis.edu.krd>
2023-09-01 04:14:21 +0300

    Login page Frontend done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,21 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <dev>
-      
-    </dev>
+    <div class="wrapper">
+    <form action="">
+        <h1>DHS</h1>
+        <h4>Doctor Helping Services</h4>
+        <div class="input-box">
+            <input type="text" placeholder="Username" required/>
+            <i class='bx bxs-user'></i>
+        </div>
+        <div class="input-box">
+            <input type="password" placeholder="Password" required/>
+            <i class='bx bxs-lock-alt' ></i>
+        </div>
+        <button type="submit" class="btn">Login</button>
+    </form>
+</div>
      
   )
 }
